8d68a722b922e223465c9587ba9ebbbf4eafbaf2
DikDns <dikdnssocial@gmail.com>
2023-06-13 14:33:12 +0700

    add: important metada: title, description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import "./globals.css";
 import { karla } from "@/services/fonts";
 
+export const metadata = {
+  title: "SITCOM",
+  description:
+    "Ekspresikan kreativitas IT kamu dengan bergabung bersama komunitas SMA Negeri 2 Bandung yang terdiri dari siswa-siswa berbakat dan berminat di bidang Teknologi Informasi",
+};
+
 export default function RootLayout({
   children,
 }: {
