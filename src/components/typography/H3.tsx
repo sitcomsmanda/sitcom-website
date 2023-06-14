c9f4939028c6e723bf1043dc3ec2e86c4946ff17
DikDns <dikdnssocial@gmail.com>
2023-06-14 14:35:00 +0700

    add: h3 letter-spacing

--- src/components/typography/H3.tsx
+++ src/components/typography/H3.tsx
@@ -1,6 +1,6 @@
 import { lato } from "@/services/fonts";
 
-const twStyle = `${lato.className} font-semibold text-sm lg:text-base xl:text-lg`;
+const twStyle = `${lato.className} font-semibold tracking-wide text-sm lg:text-base xl:text-lg`;
 
 export default function H3({ children }: { children: React.ReactNode }) {
   return <h3 className={twStyle}>{children}</h3>;
