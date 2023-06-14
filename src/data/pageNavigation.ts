9ae07015ead0f0cc9b9e979e50a8925e2dc451db
DikDns <dikdnssocial@gmail.com>
2023-06-14 15:03:51 +0700

    add: page navigation string url path

---
+++ src/data/pageNavigation.ts
@@ -0,0 +1,7 @@
+export const URL_JOIN = "https://forms.gle/bz3DuTD9ntRp2vxr9";
+export const PATH_HOME = "/";
+export const PATH_ABOUT = "/about";
+export const PATH_MEMBER = "/member";
+export const PATH_DOCUMENTATION = "/documentation";
+export const PATH_ACTIVITIES = "/activities";
+export const PATH_CONTACT = "/contact";
