5d8e394d40c37674907a8b023a0001dc788663ed
DikDns <dikdnssocial@gmail.com>
2023-06-14 11:09:35 +0700

    add: lato weight variants

--- src/services/fonts.ts
+++ src/services/fonts.ts
@@ -1,4 +1,7 @@
 import { Lato, Karla } from "next/font/google";
 
-export const lato = Lato({ subsets: ["latin"], weight: "400" });
+export const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+});
 export const karla = Karla({ subsets: ["latin"] });
