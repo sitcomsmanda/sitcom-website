3dd57be9e6f6ff592dfa8f5e657c1cdd7d71f58e
DikDns <dikdnssocial@gmail.com>
2023-06-15 09:38:37 +0700

    add: About: Profile: Tablet

--- src/layouts/About/Profile/Video.tsx
+++ src/layouts/About/Profile/Video.tsx
@@ -1,9 +1,9 @@
 export default function Video() {
   return (
-    <div className="relative w-full sm:w-1/2 h-48 rounded shadow shadow-black">
-      <div className="absolute bg-slate-950 h-3/4 -left-40 -right-40 top-6 sm:left-0 sm:right-0" />
+    <div className="relative w-full sm:w-1/2 h-48">
+      <div className="absolute bg-slate-900 h-3/4 -left-40 -right-40 top-6 sm:h-auto sm:-top-4 sm:-bottom-4 sm:rounded sm:left-8 sm:right-8" />
       <iframe
-        className="relative z-10 w-full h-full"
+        className="relative z-10 w-full h-full rounded"
         src="https://www.youtube.com/embed/ovroVcucdBU"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
