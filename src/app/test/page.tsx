7133587b0e6cf5efff00a9ce817c4496b3049d2f
DikDns <dikdnssocial@gmail.com>
2023-06-15 15:32:04 +0700

    add: test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import Input from "@/components/form/Input";
+
+export default function () {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Input type="text" />
+    </div>
+  );
+}
