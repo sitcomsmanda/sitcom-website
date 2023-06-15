1e03bcd1fa9e9d8c09621a271a076f189460b43f
DikDns <dikdnssocial@gmail.com>
2023-06-15 09:53:45 +0700

    add: About: Profile: Large Screen

--- src/layouts/About/Profile/Video.tsx
+++ src/layouts/About/Profile/Video.tsx
@@ -1,7 +1,7 @@
 export default function Video() {
   return (
-    <div className="relative w-full sm:w-1/2 h-48">
-      <div className="absolute bg-slate-900 h-3/4 -left-40 -right-40 top-6 sm:h-auto sm:-top-4 sm:-bottom-4 sm:rounded sm:left-8 sm:right-8" />
+    <div className="relative w-full sm:w-1/2 h-48 lg:h-80">
+      <div className="absolute bg-slate-900 h-3/4 -left-40 -right-40 top-6 sm:h-auto sm:-top-4 sm:-bottom-4 sm:rounded sm:left-8 sm:right-8 lg:-top-8 lg:-bottom-8" />
       <iframe
         className="relative z-10 w-full h-full rounded"
         src="https://www.youtube.com/embed/ovroVcucdBU"
