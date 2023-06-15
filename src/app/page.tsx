13e874f5554ab9f583c5464c56d277b4ad94562a
DikDns <dikdnssocial@gmail.com>
2023-06-15 13:17:26 +0700

    add: Documentation: Highlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/layouts/Home/Hero";
 import Profile from "@/layouts/About/Profile";
 import Testimonial from "@/layouts/About/Testimonial";
+import Highlight from "@/layouts/Documentation/Highlight";
 
 export default function Home() {
   return (
@@ -15,6 +16,10 @@ export default function Home() {
 
       <section className="py-20 bg-gradient-radial from-blue-300 to-blue-200">
         <Testimonial />
+      </section>
+
+      <section className="overflow-hidden py-20">
+        <Highlight />
       </section>
 
       <section className="mb-96"></section>
