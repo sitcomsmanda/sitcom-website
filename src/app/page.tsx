6057bbe0411d8a9c130d844b7fea7c5c242e5c9d
DikDns <dikdnssocial@gmail.com>
2023-06-15 11:32:31 +0700

    add: Testimonial: gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/layouts/Home/Hero";
 import Profile from "@/layouts/About/Profile";
+import Testimonial from "@/layouts/About/Testimonial";
 
 export default function Home() {
   return (
@@ -8,8 +9,12 @@ export default function Home() {
         <Hero />
       </section>
 
-      <section className="overflow-hidden mt-10">
+      <section className="overflow-hidden py-20">
         <Profile />
+      </section>
+
+      <section className="py-20 bg-gradient-radial from-green-300 to-green-200">
+        <Testimonial />
       </section>
 
       <section className="mb-96"></section>
