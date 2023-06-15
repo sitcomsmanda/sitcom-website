a989b5b986a5b7d15b2be4564e00c2103b5a0354
DikDns <dikdnssocial@gmail.com>
2023-06-15 10:09:20 +0700

    add: Home: Hero: grid align

--- src/layouts/home/Hero/Content.tsx
+++ src/layouts/home/Hero/Content.tsx
@@ -3,22 +3,27 @@ import Actions from "./Actions";
 
 export default function Content() {
   return (
-    <div className="w-full flex flex-col gap-5 sm:w-1/2 sm:pr-2 lg:gap-10 2xl:w-1/3">
-      <H1>
-        Ekspresikan
-        <br />
-        <strong className="font-bold text-green-400">Kreativitas </strong>
-        IT Kamu
-      </H1>
+    <>
+      <div className="w-full flex flex-col gap-5 sm:w-1/2 lg:gap-10 2xl:w-1/3">
+        <H1>
+          Ekspresikan
+          <br />
+          <strong className="font-bold text-green-400">Kreativitas </strong>
+          IT Kamu
+        </H1>
 
-      <H3>
-        Bergabunglah dengan komunitas anak SMA Negeri 2 Bandung yang terdiri
-        dari siswa-siswa berbakat dan berminat di bidang Teknologi Informasi.
-      </H3>
+        <H3>
+          Bergabunglah dengan komunitas anak SMA Negeri 2 Bandung yang terdiri
+          dari siswa-siswa berbakat dan berminat di bidang Teknologi Informasi.
+        </H3>
 
-      <div className="flex flex-col gap-4 lg:flex-row-reverse lg:gap-8">
-        <Actions />
+        <div className="flex flex-col gap-4 lg:flex-row-reverse lg:gap-8">
+          <Actions />
+        </div>
       </div>
-    </div>
+      <div className="hidden w-0 sm:block sm:w-1/2 sm:opacity-0">
+        PLACEHOLDER
+      </div>
+    </>
   );
 }
