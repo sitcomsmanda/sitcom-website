62faa6ec6e76fde332272bc4e8524fb1832b7172
DikDns <dikdnssocial@gmail.com>
2023-06-15 13:35:54 +0700

    add: thumbnail prop

--- src/layouts/Card/index.tsx
+++ src/layouts/Card/index.tsx
@@ -1,13 +1,15 @@
 import { H3, Text } from "@/components/typography";
 
 type cardArgs = {
+  thumbnail?: React.ReactNode;
   title: String;
   text: String;
 };
 
-export default function Card({ title, text }: cardArgs) {
+export default function Card({ thumbnail, title, text }: cardArgs) {
   return (
     <div className="w-full bg-slate-50 drop-shadow-md border border-slate-300 px-4 py-6 rounded-lg flex flex-col gap-2">
+      {thumbnail ? thumbnail : ""}
       <H3>{title}</H3>
       <Text>{text}</Text>
     </div>
