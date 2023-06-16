ecfc8d207ff0c015eaeba69270902e1a036eee17
DikDns <dikdnssocial@gmail.com>
2023-06-16 15:49:59 +0700

    add: Contact: Form action to formester

--- src/layouts/Contact/Form.tsx
+++ src/layouts/Contact/Form.tsx
@@ -5,13 +5,26 @@ import { Button } from "@/components/interactivity";
 
 export default function Form() {
   return (
-    <form className="w-full flex flex-col gap-5">
-      <TextField type="email" label="Email" id="email" name="email" />
-      <TextField type="text" label="Nama Lengkap" id="nama" name="nama" />
+    <form
+      className="w-full flex flex-col gap-5"
+      accept-charset="UTF-8"
+      action="https://app.formester.com/forms/7cd5195c-db94-4131-aeed-f956094af764/submissions"
+      method="POST"
+    >
+      <TextField required type="email" label="Email" id="email" name="email" />
+      <TextField
+        required
+        type="text"
+        label="Nama Lengkap"
+        id="nama"
+        name="nama"
+      />
       <TextArea label="Pesan" id="pesan" name="pesan" />
 
       <div className="mt-5 flex flex-col gap-4 lg:flex-row-reverse lg:gap-8">
-        <Button primary>Kirim</Button>
+        <Button primary type="submit">
+          Kirim
+        </Button>
 
         <Link href={URL_JOIN} tabIndex={-1} className="w-full">
           <Button secondary>Bergabung Sekarang</Button>
