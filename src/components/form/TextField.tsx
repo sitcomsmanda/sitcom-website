325884e89efaed89e37876dd65c9a94d9d299cfa
DikDns <dikdnssocial@gmail.com>
2023-06-16 09:54:54 +0700

    add: Form: large screen

--- src/components/form/TextField.tsx
+++ src/components/form/TextField.tsx
@@ -16,13 +16,13 @@ export default function TextField({
   name,
 }: inputType) {
   return (
-    <div className="text-base flex flex-col gap-1 w-full">
+    <div className="text-base flex flex-col gap-1 w-full lg:gap-2 lg:text-lg">
       <label htmlFor={id}>{label}</label>
       <input
         type={type}
         id={id}
         name={name}
-        className="transition-all duration-250 bg-slate-100 w-full border border-slate-400 rounded px-2 py-1 focus:outline-none focus:border-green-400 focus:bg-green-100 hover:border-slate-800"
+        className="transition-all duration-250 bg-slate-100 w-full border border-slate-400 rounded px-2 py-1 focus:outline-none focus:border-green-400 focus:bg-green-100 hover:border-slate-800 lg:px-4 lg:py-2"
       >
         {children}
       </input>
