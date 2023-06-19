867930499e9ac2e7080d236c313c39a1bd85eb6a
DikDns <dikdnssocial@gmail.com>
2023-06-19 11:25:32 +0700

    add: Layout: NavBrand

--- src/layouts/Navigation/index.tsx
+++ src/layouts/Navigation/index.tsx
@@ -1,10 +1,24 @@
+import NavBrand from "./NavBrand";
 import NavLinks from "./NavLinks";
 import NavButton from "./NavButton";
+import { fslc } from "@/modules/utils";
+
+const twStyle = fslc(`
+  md:z-20 md:fixed md:top-0 md:left-0 md:right-0
+  md:flex md:justify-between
+  md:px-8 xl:px-16
+  md:py-2
+  md:shadow md:shadow-slate-700
+  md:bg-slate-100
+`);
 
 export default function Navigation() {
   return (
     <nav>
-      <NavLinks />
+      <div className={`${twStyle}`}>
+        <NavBrand />
+        <NavLinks />
+      </div>
       <NavButton />
     </nav>
   );
