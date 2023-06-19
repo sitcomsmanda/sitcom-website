a714c91592b878e166ddbf39e72f12d42ff928e5
DikDns <dikdnssocial@gmail.com>
2023-06-19 14:14:06 +0700

    add: about page mt-14

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,8 @@ import { H1 } from "@/components/typography";
 
 export default function About() {
   return (
-    <>
-      <main className="mt-14">
-        <H1>ABOUT</H1>
-      </main>
-    </>
+    <main className="md:mt-14">
+      <H1>ABOUT</H1>
+    </main>
   );
 }
