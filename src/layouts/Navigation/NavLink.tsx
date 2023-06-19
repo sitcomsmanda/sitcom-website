1724cffce7d22024c694cd74db468d0d1a81045a
DikDns <dikdnssocial@gmail.com>
2023-06-19 11:22:15 +0700

    add: Layout: NavLink: Pathname style conditional

--- src/layouts/Navigation/NavLink.tsx
+++ src/layouts/Navigation/NavLink.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import { Url } from "next/dist/shared/lib/router/router";
+import { usePathname } from "next/navigation";
 import { lato } from "@/services/fonts";
 
 export default function NavLink({
@@ -9,10 +12,16 @@ export default function NavLink({
   children: React.ReactNode;
   href: Url;
 }) {
+  const path = usePathname();
+
   return (
     <Link
       href={href}
-      className={`transition-all duration-150 ease-in-out text-slate-700 py-1 px-2 w-full text-center rounded-full hover:bg-green-200 hover:text-green-800 active:bg-green-100 active:text-green-700`}
+      className={`${
+        path === href
+          ? `text-green-700 bg-green-300`
+          : `text-slate-700 hover:bg-green-200 hover:text-green-800 active:bg-green-100 active:text-green-700`
+      } transition-all duration-150 ease-in-out py-1 px-4 w-full text-center rounded-full `}
     >
       <h5
         className={`${lato.className} font-bold text-2xl md:text-base md:font-normal lg:text-lg`}
