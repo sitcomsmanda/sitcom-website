2e53bc24cf553f7086c59d2120cb5f13bbee0473
DikDns <dikdnssocial@gmail.com>
2023-06-19 13:53:25 +0700

    add: Layout: Home: div per node children

--- src/layouts/home/Hero/Content.tsx
+++ src/layouts/home/Hero/Content.tsx
@@ -5,17 +5,21 @@ export default function Content() {
   return (
     <>
       <div className="w-full flex flex-col gap-5 sm:w-1/2 lg:gap-10">
-        <H1>
-          Ekspresikan
-          <br />
-          <strong className="font-bold text-green-400">Kreativitas </strong>
-          IT Kamu
-        </H1>
-
-        <H3>
-          Bergabunglah dengan komunitas anak SMA Negeri 2 Bandung yang terdiri
-          dari siswa-siswa berbakat dan berminat di bidang Teknologi Informasi.
-        </H3>
+        <div>
+          <H1>
+            Ekspresikan
+            <br />
+            <strong className="font-bold text-green-400">Kreativitas </strong>
+            IT Kamu
+          </H1>
+        </div>
+        <div>
+          <H3>
+            Bergabunglah dengan komunitas anak SMA Negeri 2 Bandung yang terdiri
+            dari siswa-siswa berbakat dan berminat di bidang Teknologi
+            Informasi.
+          </H3>
+        </div>
 
         <div className="flex flex-col gap-4 lg:flex-row-reverse lg:gap-8">
           <Actions />
