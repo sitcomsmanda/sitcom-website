0642158e9b3d68443b5eeda457333ab003759168
DikDns <dikdnssocial@gmail.com>
2023-06-19 11:38:31 +0700

    add: Route: Page: Initial components

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import Navigation from "@/layouts/Navigation";
+
+export default function Contact() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
---
+++ src/app/member/page.tsx
@@ -0,0 +1,9 @@
+import Navigation from "@/layouts/Navigation";
+
+export default function Member() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
