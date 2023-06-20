b5804574b6f50efe5945165d8595b00dc0b64a85
DikDns <dikdnssocial@gmail.com>
2023-06-20 09:00:14 +0700

    add: Layout Contact

--- src/layouts/Contact/Banner.tsx
+++ src/layouts/Contact/Banner.tsx
@@ -3,12 +3,12 @@ import contactBanner from "@/assets/backgrounds/bg-contact-banner.jpg";
 
 export default function Banner() {
   return (
-    <div className="hidden sm:block sm:w-1/2 sm:h-[650px]">
+    <div className="w-full h-40 sm:w-1/2 sm:h-[650px]">
       <Image
         src={contactBanner}
         alt="Tangan memegang ponsel pintar"
         placeholder="blur"
-        className="rounded-lg object-cover w-full h-full"
+        className="brightness-50 rounded-lg object-cover w-full h-full"
       />
     </div>
   );
--- src/layouts/Contact/Content.tsx
+++ src/layouts/Contact/Content.tsx
@@ -1,11 +1,11 @@
-import { H2, H3 } from "@/components/typography";
+import { H1, H3 } from "@/components/typography";
 import Form from "./Form";
 
 export default function Content() {
   return (
     <div className="flex flex-col gap-10 w-full sm:w-1/2">
       <div className="flex flex-col gap-5">
-        <H2>Hubungi Kami</H2>
+        <H1>Hubungi Kami</H1>
 
         <H3>
           Jika anda tertarik untuk bekerja sama, silahkan isi kolom kontak di
