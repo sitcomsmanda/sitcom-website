704482bbc172891803dd7c92532904d5a2490d16
DikDns <dikdnssocial@gmail.com>
2023-06-20 09:15:45 +0700

    edit: layout nav: NavLink hover transition duration

--- src/layouts/Navigation/NavLink.tsx
+++ src/layouts/Navigation/NavLink.tsx
@@ -37,11 +37,11 @@ export default function NavLink({
     <Link
       onClick={handleClick}
       href={href}
-      className={`${
+      className={`transition-all duration-250 ease-in-out py-1 px-4 w-full text-center rounded-full ${
         path === href
           ? `text-green-700 bg-green-300`
           : `text-slate-700 hover:bg-green-200 hover:text-green-800 active:bg-green-100 active:text-green-700`
-      } transition-all duration-150 ease-in-out py-1 px-4 w-full text-center rounded-full `}
+      }`}
     >
       <h5
         className={`${lato.className} font-bold text-2xl md:text-base md:font-normal lg:text-lg`}
