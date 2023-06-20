8492de1374ffc9e80fd5cf5f4b2bca316de3f1d6
DikDns <dikdnssocial@gmail.com>
2023-06-20 14:02:53 +0700

    edit: layout home: activities import program

--- src/layouts/home/Activities/Cards.tsx
+++ src/layouts/home/Activities/Cards.tsx
@@ -1,44 +1,9 @@
-import bgWebProgramming from "@/assets/backgrounds/bg-web-programming.jpg";
-import bgSoftwareOffice from "@/assets/backgrounds/bg-software-office.jpg";
 import Card from "@/layouts/Card";
-import { Key } from "react";
-import Image, { StaticImageData } from "next/image";
-
-type activitiesType = {
-  id: Key;
-  image: StaticImageData;
-  title: String;
-  blurb: String;
-  description: String;
-};
-
-const activities = [
-  {
-    id: 1,
-    title: `Pengembangan Website`,
-    image: bgWebProgramming,
-    blurb: `Pembuatan website melalui HTML, CSS, dan JavaScript.`,
-    description: `Mengembangkan keterampilan dalam membuat website menggunakan HTML, CSS, dan JavaScript.`,
-  },
-  {
-    id: 2,
-    title: `Penggunaan Software Office`,
-    image: bgSoftwareOffice,
-    blurb: `Microsoft Word, Excel, dan Software Office Lainnya`,
-    description: `Memperluas pemahaman penggunaan Software Office untuk produktivitas yang lebih tinggi`,
-  },
-];
-
-function GenerateImage(src: StaticImageData, alt: string) {
-  return (
-    <Image
-      src={src}
-      alt={alt}
-      placeholder="blur"
-      className="object-cover h-20 w-full"
-    />
-  );
-}
+import {
+  activitiesType,
+  activities,
+  GenerateImage,
+} from "@/layouts/Activities/Program/ProgramList";
 
 function GenerateCards(data: activitiesType[]) {
   return data.map((x) => (
