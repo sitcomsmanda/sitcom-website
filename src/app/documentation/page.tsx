7a7eac4982bb6653d48a13d8780513a28e6af43a
DikDns <dikdnssocial@gmail.com>
2023-06-20 13:35:29 +0700

    add: Documentation Page

--- src/app/documentation/page.tsx
+++ src/app/documentation/page.tsx
@@ -1,9 +1,21 @@
-import { H1 } from "@/components/typography";
+import Documentation from "@/layouts/Documentation";
+import VideoList from "@/layouts/Documentation/VideoList";
+import Contact from "@/layouts/Home/Contact";
 
-export default function Documentation() {
+export default function DocumentationPage() {
   return (
-    <main className="mt-14">
-      <H1>Documentation</H1>
+    <main>
+      <section className="min-h-screen pb-20 md:mt-14">
+        <Documentation />
+      </section>
+
+      <section className="py-20 bg-gradient-radial from-slate-300 to-slate-100">
+        <VideoList />
+      </section>
+
+      <section className="py-20">
+        <Contact />
+      </section>
     </main>
   );
 }
