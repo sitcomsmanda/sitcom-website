659c829eb9bdf815a746a21a26b4146ccff20097
DikDns <dikdnssocial@gmail.com>
2023-06-21 08:34:21 +0700

    edit: Refactor: Layout Home Documentation

--- src/layouts/Home/Documentation/Video.tsx
+++ src/layouts/Home/Documentation/Video.tsx
@@ -1,9 +1,18 @@
 import YoutubePlayer from "@/components/media/YoutubePlayer";
+import { fslc } from "@/modules/utils";
+
+const bgVideoTwStyle = fslc(`
+  bg-slate-900 h-3/4 
+  absolute -left-40 -right-40 top-6 
+  sm:h-auto sm:rounded
+  sm:-top-4 sm:-bottom-4 sm:left-8 sm:right-8
+  lg:-top-8 lg:-bottom-8
+`);
 
 export default function Video() {
   return (
     <div className="relative w-full sm:w-1/2 h-48 lg:h-80">
-      <div className="absolute bg-slate-900 h-3/4 -left-40 -right-40 top-6 sm:h-auto sm:-top-4 sm:-bottom-4 sm:rounded sm:left-8 sm:right-8 lg:-top-8 lg:-bottom-8" />
+      <div className={bgVideoTwStyle} />
       <YoutubePlayer
         className="relative z-10 w-full h-full rounded"
         videoId="G-owMw1-Es0"
